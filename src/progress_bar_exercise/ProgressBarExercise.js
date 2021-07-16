fc30655ebf05fa8f462f2261df61b9e9ca48b922
Ayman A <ayman.ali@nutrien.com>
2021-07-15 20:26:10 -0700

    refactor: encapsulate progress logic

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Exercise from "../exercise/Exercise";
 import ProgressBar from "./componnts/ProgressBar";
 import { Styled } from "./styled-components";
@@ -18,61 +18,16 @@ const ProgressBarExercise = () => {
 export default ProgressBarExercise;
 
 // ----------------------------------------------------------------------------------
-let progressIntervalReference;
-let finishTimerReference;
-const tickValue = 10;
-const defaultProgressStep = 90 / (15000 / tickValue);
+
 const Solution = () => {
   const [loading, setLoading] = useState(false);
   const [finished, setFinished] = useState(false);
-  const [progress, setProgress] = useState(0);
-  const [progressStep, setProgressStep] = useState(defaultProgressStep);
-
-  useEffect(() => {
-    if (loading) {
-      progressIntervalReference = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress < 90) {
-            return prevProgress + progressStep;
-          } else {
-            clearInterval(progressIntervalReference);
-            return prevProgress;
-          }
-        });
-      }, tickValue);
-    } else {
-      clearInterval(progressIntervalReference);
-    }
-
-    if (finished && !loading) {
-      progressIntervalReference = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress < 100) {
-            return prevProgress + progressStep;
-          } else {
-            clearInterval(progressIntervalReference);
-            finishTimerReference = setTimeout(() => {
-              setProgress(0);
-              clearTimeout(finishTimerReference);
-            }, 3000);
-            return 100;
-          }
-        });
-      }, tickValue);
-    }
-    return () => {
-      clearInterval(progressIntervalReference);
-      clearTimeout(finishTimerReference);
-    };
-  }, [loading, progressStep, finished]);
 
   const handleStartRequest = () => {
-    setProgressStep(defaultProgressStep);
     setLoading(true);
   };
 
   const handleFinihRequest = () => {
-    setProgressStep((100 - progress) / (1000 / tickValue));
     setLoading(false);
     setFinished(true);
   };
@@ -80,7 +35,7 @@ const Solution = () => {
   return (
     <div>
       <div>Add solution here</div>
-      <ProgressBar percent={progress} />
+      <ProgressBar loading={loading} finished={finished} />
       <Styled.Container>
         <Styled.Button color="#2ebb9c" onClick={handleStartRequest}>{`${
           loading ? "Loading ..." : "Start Request"
--- src/progress_bar_exercise/componnts/ProgressBar.js
+++ src/progress_bar_exercise/componnts/ProgressBar.js
@@ -1,8 +1,61 @@
-import React from "react";
-import {Styled} from './styled-components';
+import React, { useEffect, useState } from "react";
+import { Styled } from "./styled-components";
 
-const ProgressBar = ({ percent }) => {
-  return <Styled.ProgressBar percent={percent} />;
+let progressIntervalReference;
+let finishTimerReference;
+const DEFAULT_TIMER_VALUE = 100;
+const DEFAULT_PROGRESS_VALUE = 90 / (15000 / DEFAULT_TIMER_VALUE);
+
+const ProgressBar = ({ loading, finished }) => {
+  const [progress, setProgress] = useState(0);
+  const [progressStep, setProgressStep] = useState(DEFAULT_PROGRESS_VALUE);
+
+  useEffect(() => {
+    if (finished && progress < 90) {
+      setProgressStep((100 - progress) / (1000 / DEFAULT_TIMER_VALUE));
+    }
+  }, [finished, progress]);
+
+  useEffect(() => {
+    if (loading) {
+      setProgressStep(DEFAULT_PROGRESS_VALUE);
+      progressIntervalReference = setInterval(() => {
+        setProgress((prevProgress) => {
+          if (prevProgress < 90) {
+            return prevProgress + progressStep;
+          } else {
+            clearInterval(progressIntervalReference);
+            return prevProgress;
+          }
+        });
+      }, DEFAULT_TIMER_VALUE);
+    } else {
+      clearInterval(progressIntervalReference);
+    }
+
+    if (finished && !loading) {
+      progressIntervalReference = setInterval(() => {
+        setProgress((prevProgress) => {
+          if (prevProgress < 100) {
+            return prevProgress + progressStep;
+          } else {
+            clearInterval(progressIntervalReference);
+            finishTimerReference = setTimeout(() => {
+              setProgress(0);
+              clearTimeout(finishTimerReference);
+            }, 3000);
+            return 100;
+          }
+        });
+      }, DEFAULT_TIMER_VALUE);
+    }
+    return () => {
+      clearInterval(progressIntervalReference);
+      clearTimeout(finishTimerReference);
+    };
+  }, [loading, progressStep, finished]);
+
+  return <Styled.ProgressBar percent={progress} />;
 };
 
 export default ProgressBar;
